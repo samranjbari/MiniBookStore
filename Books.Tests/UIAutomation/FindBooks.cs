c67715c599c565864462e7bcf279d704000834b1
samranjbari <samranjbari@gmail.com>
2014-03-06 13:42:57 -0500

    initial commit for UI Automation

---
+++ Books.Tests/UIAutomation/FindBooks.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace Books.Tests.UIAutomation
+{
+    [TestClass]
+    public class FindBooks
+    {
+        private IWebDriver driver;
+        private string hubURL = "http://10380d-deegan:4444/wd/hub/";
+        private string baseURL;
+
+        [TestInitialize]
+        public void Init()
+        {
+            baseURL = "http://10380d-deegan:801/";
+
+            DesiredCapabilities capability = DesiredCapabilities.InternetExplorer();
+            
+            driver = new RemoteWebDriver(new Uri(hubURL), capability);
+        }
+
+        [TestMethod]
+        public void GoToPage()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/Home/FindBook");
+
+            Assert.AreEqual(driver.Title, "Find Books");
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+    }
+}
